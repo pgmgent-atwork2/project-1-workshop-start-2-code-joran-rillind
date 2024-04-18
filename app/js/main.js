d4a22aa39580ad60a536f4a7abba5d9bd54f399f
pgm-jorancoen <joran.coen@student.arteveldehs.be>
2024-04-18 15:46:12 +0200

    init commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,44 +1,58 @@
 const bird = document.getElementById('bird');
-const pipe = document.getElementById('pipe');
+const pipe1 = document.getElementById('pipe1');
+const pipe2 = document.getElementById('pipe2');
 
-let birdTop = 250;
-let gravity = 1.5;
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        flap();
+    }
+});
+
 let isJumping = false;
-let gameSpeed = 10;
-let pipeLeft = 400;
+let isGameOver = false;
+let gameSpeed = 15;
+let pipeLeft = 1500;
+let gravity = 0.2;
+let velocity = 0;
+let position = 250;
 
-function jump() {
-    if (isJumping) return;
-    isJumping = true;
-
-    let jumpInterval = setInterval(() => {
-        if (birdTop <= 0) {
-            clearInterval(jumpInterval);
-            isJumping = false;
-        } else {
-            birdTop -= 5;
-            bird.style.top = birdTop + 'px';
-        }
-    }, 20);
+function flap() {
+    velocity = -5;
 }
 
-function fall() {
-    let fallInterval = setInterval(() => {
-        if (birdTop >= document.documentElement.clientHeight - 40) {
-            clearInterval(fallInterval);
-            gameOver();
-        } else {
-            birdTop += gravity;
-            bird.style.top = birdTop + 'px';
-        }
-    }, 20);
+function gameLoop() {
+    velocity += gravity;
+    position += velocity;
+    if (position >= 875) {
+        position = 875;
+        velocity = 0;
+        isJumping = false;
+    }
+    bird.style.top = position + 'px';
+
+    if (isColliding(bird, pipe1) || isColliding(bird, pipe2) || position === 875 || position === 0) {
+        gameOver();
+    }
+    
+    requestAnimationFrame(gameLoop);
 }
 
-function movePipe() {
+function isColliding(element1, element2) {
+    const rect1 = element1.getBoundingClientRect();
+    const rect2 = element2.getBoundingClientRect();
+    return !(
+        rect1.bottom < rect2.top ||
+        rect1.top > rect2.bottom ||
+        rect1.right < rect2.left ||
+        rect1.left > rect2.right
+    );
+}
+
+function movePipe(pipe) {
     let pipeInterval = setInterval(() => {
         if (pipeLeft < -80) {
-            pipeLeft = 400;
-            pipe.style.height = Math.floor(Math.random() * 200) + 100 + 'px';
+            pipeLeft = 1500;
+            pipe.style.height = Math.floor(Math.random() * 200) + 100 + 'px'; 
         } else {
             pipeLeft -= 5;
             pipe.style.left = pipeLeft + 'px';
@@ -47,15 +61,19 @@ function movePipe() {
 }
 
 function gameOver() {
+    isGameOver = true;
     alert('Game Over!');
-    window.location.reload(); 
+    resetGame();
 }
 
-document.addEventListener('keydown', (event) => {
-    if (event.code === 'Space') {
-        jump();
-    }
-});
+function resetGame() {
+    position = 250;
+    velocity = 0;
+    pipeLeft = 1500;
+    bird.style.top = '0px';
+    isGameOver = false;
+}
 
-movePipe();
-fall();
+movePipe(pipe1);
+movePipe(pipe2);
+gameLoop();
